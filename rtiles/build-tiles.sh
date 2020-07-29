dca51681d6d5b562d3d54adc9e15bd2985dd7406
Ricardo Morin <morinricardo@gmail.com>
2020-07-29 15:15:28 -0700

    Update script

--- rtiles/build-tiles.sh
+++ rtiles/build-tiles.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
 # exit on error
 set -e
-# cd /tmp
+
+# Timestamp function
+timestamp() {
+  date
+}
+
+# Create color table file
+echo $2 | sed "s/:/: /g" | tr "-" "\n" > color.txt
+
+# Get source file
 filename=$1
-echo "get https://geoportalp.s3-us-west-2.amazonaws.com/files/$filename"
+echo $(timestamp): "get https://geoportalp.s3-us-west-2.amazonaws.com/files/$filename"
 curl -s -O https://geoportalp.s3-us-west-2.amazonaws.com/files/$filename
 name=${filename%.*}
 namelc="$(echo "$name" | tr '[:upper:]' '[:lower:]')"
-echo "derived name: $namelc"
+echo $(timestamp): "derived name: $namelc"
 
-echo "remove old tiles from vtiles/$namelc"
-# aws s3 rm s3://geoportalp --dryrun --recursive --exclude "*" --include "vtiles/$namelc*"
-echo "upload generated tiles to vtiles/$namelc"
-# aws s3 cp vtiles s3://geoportalp/vtiles --acl "public-read" --content-encoding "gzip" --dryrun --recursive
+# Generate tiles
+echo $(timestamp): "create virtual raster with colors"
+gdaldem color-relief -alpha -exact_color_entry -of vrt $filename color.txt temp.vrt
+
+echo $(timestamp): "generate raster tiles"
+gdal2tiles.py --profile=mercator -q -z 4-10 temp.vrt rtiles/bosques
+echo -e "\n"
+
+# Copy tiles to S3
+echo $(timestamp): "remove old tiles from rtiles/$namelc"
+aws s3 rm s3://geoportalp --quiet --recursive --exclude "*" --include "rtiles/$namelc*"
+echo $(timestamp): "upload generated tiles to rtiles/$namelc"
+aws s3 cp rtiles s3://geoportalp/rtiles --acl "public-read" --quiet --recursive
+echo $(timestamp): "finished processing"
