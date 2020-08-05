ea35e804611412ee0b3d8b49c0edb413a92ade9c
Ricardo Morin <morinricardo@gmail.com>
2020-08-04 17:35:24 -0700

    Add setup menu

--- scripts/setupaws.sh
+++ scripts/setupaws.sh
@@ -1,57 +1,90 @@
 #!/bin/bash
-# aws iam create-user --user-name geoportalp
+AWS_ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
+while [[ "$n" != "q" ]]; do
 
-# aws s3api create-bucket --bucket "geoportalp-files" --create-bucket-configuration LocationConstraint="us-east-2" --acl "public-read"
+  echo
+  echo "***********************************************"
+  echo
+  echo "Setup AWS environment to support geoportalp"
+  echo "Using AWS account: $AWS_ACCOUNT"
+  echo
+  echo "***********************************************"
+  echo
+  echo "Select setup option below:"
+  echo
+  echo "  1) Create API user, S3 bucket, create and attach policies"
+  echo "  2) Create compute environment and container repos"
+  echo "  3) Push container images"
+  echo "  4) Create job queues and job definitions"
+  echo "  5) Do all of the above in sequence"
+  echo "  q) Quit (no changes)"
+  echo
 
-# aws s3api put-bucket-cors --bucket "geoportalp-files" --cors-configuration file://cors.json
+  read n
 
-# aws iam create-policy --policy-name geoportalp --policy-document file://api-access-policy.json
+  if [[ $n == 1 || $n == 5 ]]; then
+    echo "Create API user"
+    # aws iam create-user --user-name geoportalp
 
-# aws iam attach-user-policy --policy-arn "arn:aws:iam::052280825519:policy/geoportalp" --user-name "geoportalp"
+    echo "Create S3 bucket"
+    # aws s3api create-bucket --bucket "geoportalp-files" --create-bucket-configuration LocationConstraint="us-east-2" --acl "public-read"
+    # aws s3api put-bucket-cors --bucket "geoportalp-files" --cors-configuration file://cors.json
 
-# TODO: create roles: AWSBatchServiceRole & ecsInstanceRole (and attach policies)
+    echo "Create and attach API user access policy"
+    # aws iam create-policy --policy-name geoportalp --policy-document file://api-access-policy.json
+    # aws iam attach-user-policy --policy-arn "arn:aws:iam::$AWS_ACCOUNT:policy/geoportalp" --user-name "geoportalp"
 
-# Create AWSBatchServiceRole and ecsInstanceRole
-# aws iam create-role --role-name AWSBatchServiceRole --assume-role-policy-document file://batch-role-trust-policy.json
-# aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole --role-name AWSBatchServiceRole
-# aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole --role-name AWSBatchServiceRole
-# aws iam create-role --role-name ecsInstanceRole --assume-role-policy-document file://ecs-role-trust-policy.json
-# aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole --role-name ecsInstanceRole
-# aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role --role-name ecsInstanceRole
+    echo "Create AWSBatchServiceRole and ecsInstanceRole"
+    # aws iam create-role --role-name AWSBatchServiceRole --assume-role-policy-document file://batch-role-trust-policy.json
+    # aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole --role-name AWSBatchServiceRole
+    # aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole --role-name AWSBatchServiceRole
+    # aws iam create-role --role-name ecsInstanceRole --assume-role-policy-document file://ecs-role-trust-policy.json
+    # aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole --role-name ecsInstanceRole
+    # aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role --role-name ecsInstanceRole
 
-# Grab default vpc
-defaultvpc=$(aws ec2 describe-vpcs --filter Name="is-default",Values="true" | jq '.Vpcs | .[0] | .VpcId')
+  fi
+  if [[ $n == 2  ||  $n == 5 ]]; then
+    echo "Create compute environment"
+    # Grab default vpc
+    defaultvpc=$(aws ec2 describe-vpcs --filter Name="is-default",Values="true" | jq '.Vpcs | .[0] | .VpcId')
+    # Get subnets associated with default vpc
+    subnets=$(aws ec2 describe-subnets --filter Name="vpc-id",Values=$defaultvpc | jq '{subnets: [.Subnets[] | .SubnetId]}')
+    # Get default securiy group for default vpc
+    securitygroups=$(aws ec2 describe-security-groups --filter Name="vpc-id",Values=$defaultvpc Name="group-name",Values="default" | jq ".SecurityGroups[] | select(.VpcId==$defaultvpc) | [.GroupId]")
 
-# Get subnets assiciated with default vpc
-subnets=$(aws ec2 describe-subnets --filter Name="vpc-id",Values=$defaultvpc | jq '{subnets: [.Subnets[] | .SubnetId]}')
+    cat compute-environment.json | jq ".computeResources += $subnets |
+      .computeResources += {securityGroupIds: $securitygroups} |
+      .computeResources += {spotIamFleetRole: \"arn:aws:iam::$AWS_ACCOUNT:role/aws-ec2-spot-fleet-role\"} |
+      .serviceRole += \"arn:aws:iam::$AWS_ACCOUNT:role/AWSBatchServiceRole\"" > compute-environment-updated.json
 
-# Get default securiy group for default vpc
-securitygroups=$(aws ec2 describe-security-groups --filter Name="vpc-id",Values=$defaultvpc Name="group-name",Values="default" | jq ".SecurityGroups[] | select(.VpcId==$defaultvpc) | [.GroupId]")
+    # aws batch create-compute-environment --cli-input-json file://compute-environment-updated.json
 
-echo $subnets
-echo $defaultvpc
-echo $securitygroups
+    echo "Create container repos"
+    #aws ecr create-repository --repository-name geoportalp-rtiles
+    #aws ecr create-repository --repository-name geoportalp-vtiles
 
-#cat compute-environment.json | jq ". + {computeResources: $subnets} + {computeResources: {securityGroupIds: $securitygroups}}"
+  fi
+  if [[ $n == 3  ||  $n == 5 ]]; then
+    echo "Build and tag container images"
+    region=$(aws configure get region)
+    aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.$region.amazonaws.com
+    # docker build -t geoportalp-rtiles ../rtiles
+    # docker build -t geoportalp-vtiles ../vtiles
 
-cat compute-environment.json | jq ".computeResources += $subnets | .computeResources += {securityGroupIds: $securitygroups}" > compute-environment-updated.json
+    # docker tag geoportalp-rtiles:latest $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/geoportalp-rtiles:latest
+    # docker tag geoportalp-vtiles:latest $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/geoportalp-vtiles:latest
+    echo "Push container images"
+    # docker push $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/geoportalp-rtiles:latest
+    # docker push $AWS_ACCOUNT.dkr.ecr.us-east-2.amazonaws.com/geoportalp-vtiles:latest
+  fi
+  if [[ $n == 4  ||  $n == 5 ]]; then
+    echo "Create job queues"
 
-# aws batch create-compute-environment --cli-input-json file://compute-environment-updated.json
+    echo "Create job definitions"
 
-# Create container repos
-#aws ecr create-repository --repository-name geoportalp-rtiles
-#aws ecr create-repository --repository-name geoportalp-vtiles
-
-# Push container images
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 052280825519.dkr.ecr.us-east-2.amazonaws.com
-
-# docker build -t geoportalp-rtiles ../rtiles
-# docker build -t geoportalp-vtiles ../vtiles
-
-# docker tag geoportalp-rtiles:latest 052280825519.dkr.ecr.us-east-2.amazonaws.com/geoportalp-rtiles:latest
-# docker tag geoportalp-vtiles:latest 052280825519.dkr.ecr.us-east-2.amazonaws.com/geoportalp-vtiles:latest
-
-docker push 052280825519.dkr.ecr.us-east-2.amazonaws.com/geoportalp-rtiles:latest
-docker push 052280825519.dkr.ecr.us-east-2.amazonaws.com/geoportalp-vtiles:latest
-
-# TODO: create job queues and job definitions
+  fi
+  if [[ $n == 5 ]]; then
+    break
+  fi
+done
+echo "Done"
