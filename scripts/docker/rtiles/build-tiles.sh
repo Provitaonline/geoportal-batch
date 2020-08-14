eda603cd4ed1cd09a0c002699a70a4f109db3a1c
Ricardo Morin <morinricardo@gmail.com>
2020-08-13 18:22:58 -0700

    Check if another job is running on same file

--- scripts/docker/rtiles/build-tiles.sh
+++ scripts/docker/rtiles/build-tiles.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 # exit on error
 set -e
+
 source ./config.sh
 
 baseurl="https://$BUCKET.s3-$REGION.amazonaws.com"
+jobqueue="geoportalp-rtiles"
 
-# Timestamp function
-timestamp() {
-  date
-}
+source ./check-running-jobs.sh
 
 # Create color table file
 echo $2 | sed "s/:/: /g" | tr "-" "\n" > color.txt
 
 # Get source file
 filename=$1
-echo $(timestamp): "$baseurl/files/$filename"
+echo "$baseurl/files/$filename"
 curl -s -O $baseurl/files/$filename
 name=${filename%.*}
 namelc="$(echo "$name" | tr '[:upper:]' '[:lower:]')"
-echo $(timestamp): "derived name: $namelc"
+echo "derived name: $namelc"
 
 # Generate tiles
-echo $(timestamp): "create virtual raster with colors"
+echo "create virtual raster with colors"
 if [ $3 = "exact" ]; then EXACT="-exact_color_entry"; else EXACT=""; fi
 
 gdaldem color-relief -alpha $EXACT -of vrt $filename color.txt temp.vrt
 
-echo $(timestamp): "generate raster tiles"
+echo "generate raster tiles"
 gdal2tiles.py --profile=mercator -q -z 4-10 temp.vrt rtiles/bosques
 echo -e "\n"
 
 # Copy tiles to S3
-echo $(timestamp): "remove old tiles from rtiles/$namelc"
+echo "remove old tiles from rtiles/$namelc"
 aws s3 rm s3://$BUCKET --quiet --recursive --exclude "*" --include "rtiles/$namelc*"
-echo $(timestamp): "upload generated tiles to rtiles/$namelc"
+echo "upload generated tiles to rtiles/$namelc"
 aws s3 cp rtiles s3://$BUCKET/rtiles --acl "public-read" --quiet --recursive
-echo $(timestamp): "finished processing"
+echo "finished processing"
