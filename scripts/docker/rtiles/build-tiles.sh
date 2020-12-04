6747bcbd17875de7b133d29932707ca3f4ed6855
Ricardo Morin <morinricardo@gmail.com>
2020-12-03 17:58:47 -0800

    Fix target name

--- scripts/docker/rtiles/build-tiles.sh
+++ scripts/docker/rtiles/build-tiles.sh
@@ -24,7 +24,7 @@ if [ $3 = "exact" ]; then EXACT="-exact_color_entry"; else EXACT=""; fi
 gdaldem color-relief -alpha $EXACT -of vrt $filename color.txt temp.vrt
 
 echo "generate raster tiles"
-gdal2tiles.py --profile=mercator -q -z 4-10 temp.vrt rtiles/bosques
+gdal2tiles.py --profile=mercator -q -z 4-10 temp.vrt rtiles/$namelc
 echo -e "\n"
 
 # Copy tiles to S3
