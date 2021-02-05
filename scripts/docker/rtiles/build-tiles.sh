01c03fc1a04bed876f3d55982213bb5b25d800be
Ricardo Morin <morinricardo@gmail.com>
2021-02-04 18:17:50 -0800

    Geotiff files relocated

--- scripts/docker/rtiles/build-tiles.sh
+++ scripts/docker/rtiles/build-tiles.sh
@@ -17,11 +17,17 @@ name=${filename%.*}
 namelc="$(echo "$name" | tr '[:upper:]' '[:lower:]')"
 echo "derived name: $namelc"
 
+# Unzip file
+echo "unzipping $filename"
+unzip $filename
+echo "go to directory $name"
+cd $name
+
 # Generate tiles
-echo "create virtual raster with colors"
+echo "create virtual raster with colors from $name.tif"
 if [ $3 = "exact" ]; then EXACT="-nearest_color_entry"; else EXACT=""; fi
 
-gdaldem color-relief -alpha $EXACT -of vrt $filename color.txt temp.vrt
+gdaldem color-relief -alpha $EXACT -of vrt "$name.tif" color.txt temp.vrt
 
 echo "generate raster tiles"
 gdal2tiles.py --processes=2 --profile=mercator -q -z 4-10 temp.vrt rtiles/$namelc
